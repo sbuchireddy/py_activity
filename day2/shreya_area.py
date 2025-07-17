355f273b2537dd56a4cc3fe4e10e06f3fe34a25e
sbuchireddy <shreya.buchireddy@coforge.com>
2025-07-17 16:45:43 +0530

    day2

---
+++ day2/shreya_area.py
@@ -0,0 +1,6 @@
+#to calculate the area of the rectangle, the output should be an integer
+l=12.35 #length
+w=14.67 #width
+area=l*w #formula to calculate length*width
+print("Area=",int(area))
+Area= 181
---
+++ day2/shreya_greeting.py
@@ -0,0 +1,7 @@
+data="Luna42Kai3.14True#Knight"
+print("Name:",data[0:4].upper())
+print("42+8=",int(data[4:6])+8)
+print("3.14*2=",float(data[9:13])*2)
+rt=data[-6:]
+print("Reversed Title:",rt[::-1])
+print("Reversed Title:",data[-6:][::-1])
