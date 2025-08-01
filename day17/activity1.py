9583bfa373e547dc17b917c8a709c6b903b7152a
sbuchireddy <shreya.buchireddy@coforge.com>
2025-08-01 12:50:15 +0530

    org2

--- day17/activity1.py
+++ day17/activity1.py
@@ -11,5 +11,7 @@ for num in range(2,n+1):
 n=int(input("enter a number:"))
 sieve=list(range(2,n+1))
 print("initial list:",sieve)
-#eliminating
+#eliminating multiples of 2
+sieve=[x for x in sieve if x==2 or x%2 !=0]
+print("iteration 1 after eliminating multiples of 2:",sieve)
 
