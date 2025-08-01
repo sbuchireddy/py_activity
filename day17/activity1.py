0026ef135378b22839a7d796cd25eec556a8a37d
sbuchireddy <shreya.buchireddy@coforge.com>
2025-08-01 12:44:52 +0530

    org1

--- day17/activity1.py
+++ day17/activity1.py
@@ -1,3 +1,4 @@
+'''
 n=int(input("enter a number:"))
 for num in range(2,n+1):
 	count=0
@@ -6,3 +7,9 @@ for num in range(2,n+1):
 			count=count+1
 	if count==2:
 		print(num,',',end=' ')
+'''
+n=int(input("enter a number:"))
+sieve=list(range(2,n+1))
+print("initial list:",sieve)
+#eliminating
+
